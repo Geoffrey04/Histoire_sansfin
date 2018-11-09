0c02836c1bb3fa6673ffe38653b496ebf279e5b1
Geoffrey04 <43751762+Geoffrey04@users.noreply.github.com>
2018-11-09 10:21:33 +0100

    Début_Histoiresansfin

---
+++ appli.js
@@ -0,0 +1,14 @@
+var noms = ['Michel Varuk', 'Jim Carrey', 'Emma Watson','Renaud', 'Albert Einstein', 'Vivaldi','Eminem', 'Paul Rudd', 'Jacques', 'Bernadatte',
+            'Jhonny Depp', 'The Rock', 'Julien Lepers', 'Hugh Jackmann', 'Gotaga' , 'Philippe Etchebest','Jacquies', 'Michel', 'Bill Gates', 'HP Lovecraft',
+            'Léa', 'Nina'];
+
+var objets = ['Ordinateur', 'Téléphone', 'Téléphone Portable','Montre','Ordinateur','Dé à Coudre', 'Décapsuleur', 'Ballon de foot', 'Clavier','Tasse',
+              'Piece jaune','Lunette de soleil','fourchette','sabre','cuilliere', 'manette','clavier','avion','miche de pain','vélo','marteau'];
+
+var température = ['10°','30°','37,2°','100°','-5°','-50°','5°','12°','25°','37°','25°','69°','18°','-30°','-40°','70°','28°','24°','22°'];
+
+var Lieux =['Floyon','Wignehies','Barcelona','Paris','Tokyo','Reykjavik','Bondy','Rio de Janeiro','Big Ben','Mont Fuji','Mc Do','KFC','Montcuq','Chute du Niagara',
+             'HidePark','Burger King',"Palais de l'élysée",'Mairie de Paris','Mons','Trélon','Le touquet','Fourmies'];
+
+var verbes = ['Coder','Copier','Coordonner','Avoir','aimer','être','Passionner','Soulever','évoluer','nager','monter','développer','jouer','voter','arnarquer',
+              'manger',"S'envoyer",'Enfourner','Accomplir','Communiquer'];
